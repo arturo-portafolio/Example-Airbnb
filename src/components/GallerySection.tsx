90181b4e57f3949f654927c8df6e2fbc9729d58f
Arturo Castro <coding.portafolio@outlook.es>
2025-12-08 10:41:48 +0200

    Update GallerySection.tsx

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -6,7 +6,7 @@ import gallery2 from "@/assets/gallery-2.jpg";
 import gallery3 from "@/assets/gallery-3.jpg";
 import gallery4 from "@/assets/gallery-4.jpg";
 import gallery5 from "@/assets/gallery-5.jpg";
-import gallery6 from "@/assets/gallery-5.jpg";
+import gallery6 from "@/assets/gallery-6.jpg";
 
 const galleryImages = [
   { src: gallery1, alt: "Sala de estar con vista a la ciudad" },
